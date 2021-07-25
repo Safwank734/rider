5870cce8b7d397b53bc3766f09ef391f8f88e5ca
Safwank734 <72594069+Safwank734@users.noreply.github.com>
2021-07-25 12:15:08 +0530

    Update config.dart

--- lib/const/config.dart
+++ lib/const/config.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:rider_app/models/all_users.dart';
 
-String mapKey="AIzaSyCO7v1ufgZlZex2333GOIlKsZtUZaFGT_Y";
+
 
 User firebaseUser;
 
